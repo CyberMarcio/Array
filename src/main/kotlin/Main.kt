75fe40f39ec919ab4f38be36b0be93317711c53d
marcio <viana_marcio@outlook.com>
2022-04-09 01:48:53 -0300

    Show how uses arrays : arrayOf and intArrayOf

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+   var nomes = arrayOf("Jamilton", "Letícia", "Mariana", "José")
+    var numero = intArrayOf(20, 30, 40)
+
+
+    //nomes[0] = "Pedro"
+
+
+    println(numero[0])
+
+}
